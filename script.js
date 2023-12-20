16d6eca8d97b01cc3f78a434d030d50bedfae695
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-20 13:21:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function interactiveSearch(){
 	let divContainer=document.getElementById("searchContainer");
 	// let cssClassName=
 	divContainer.classList.toggle("search");
-	var searchInput = document.getElementByClassName('input');
+	var searchInput = document.getElementsByClassName('input');
     searchInput.focus();
 }
