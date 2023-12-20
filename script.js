cff4889c41b87af58bbaa04c266277f5e3423384
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-20 13:43:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //your JS code here. If required.
 function interactiveSearch(){
 	let divContainer=document.getElementById("searchContainer");
-	// let cssClassName=
 	divContainer.classList.toggle("active");
 	var searchInput = document.getElementById('searchInput');
     searchInput.focus();
